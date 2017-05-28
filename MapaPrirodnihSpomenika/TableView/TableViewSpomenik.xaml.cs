27db3c9b810197a0362dc591a892b3f3bd657dac
karpic <arsenije_karpic@uns.ac.rs>
2017-05-28 23:22:44 +0200

    zapocet rad na implementaciji filtera tabela

--- MapaPrirodnihSpomenika/TableView/TableViewSpomenik.xaml.cs
+++ MapaPrirodnihSpomenika/TableView/TableViewSpomenik.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MapaPrirodnihSpomenika.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +19,87 @@ namespace MapaPrirodnihSpomenika.TableView
     /// <summary>
     /// Interaction logic for TableViewSpomenik.xaml
     /// </summary>
-    public partial class TableViewSpomenik : Window
+    public partial class TableViewSpomenik : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        CollectionViewSource itemSourceList;
+        ICollectionView Itemlist;
+        private string _oznaka;
+        public string Oznaka
+        {
+            get
+            {
+                return _oznaka;
+            }
+            set
+            {
+                if (value != _oznaka)
+                {
+                    _oznaka = value;
+                    OnPropertyChanged("Oznaka");
+                }
+            }
+        }
+        private string _ime;
+        public string Ime
+        {
+            get
+            {
+                return _ime;
+            }
+            set
+            {
+                if (value != _ime)
+                {
+                    _ime = value;
+                    OnPropertyChanged("Ime");
+                }
+            }
+        }
+        private string _tip;
+        public string Tip
+        {
+            get
+            {
+                return _tip;
+            }
+            set
+            {
+                if (value != _tip)
+                {
+                    _tip = value;
+                    OnPropertyChanged("Tip");
+                }
+            }
+        }
+
         public TableViewSpomenik()
         {
             InitializeComponent();
+            this.textBoxOznaka.DataContext = this;
+            this.textBoxIme.DataContext = this;
+            this.textBoxTip.DataContext = this;
+            itemSourceList = new CollectionViewSource() { Source = MainWindow.Spomenici };
+
             
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            Itemlist = itemSourceList.View;
+            var yourCostumFilter = new Predicate<object>(item => ((Spomenik)item).Oznaka.Contains(_oznaka) && ((Spomenik)item).Ime.Contains(Ime));
+            
+            Itemlist.Filter = yourCostumFilter;
+            
+            grdViewSpomenik.ItemsSource = Itemlist;
         }
     }
 }
